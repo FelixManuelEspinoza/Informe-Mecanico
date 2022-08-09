4f51aa165a487fda2959d1aeba140d31b6a3edd6
FelixManuelEspinoza <81267662+FelixManuelEspinoza@users.noreply.github.com>
2022-08-09 01:40:34 -0300

    Agregar archivos de proyecto.

---
+++ InformeBD/Data/BDcontext.cs
@@ -0,0 +1,29 @@
+﻿using InformeBD.Data.entidades;
+using InformeBD.entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformeBD.Data
+{
+    public class BDcontext : DbContext
+    {
+
+        public DbSet <Mecanico> Mecanicos { get; set; } 
+
+        public DbSet <Cliente> Clientes { get; set; }
+
+        public DbSet <Vehiculo> Vehiculos { get; set; }
+
+        public DbSet <Secretario> Secretarios { get; set; }
+
+
+        public BDcontext(DbContextOptions options) : base(options)
+        {
+        
+        }
+    }
+}
---
+++ InformeBD/Data/entidades/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformeBD.entidades
+{
+    [Index (nameof(DNI), Name= "Numero de DNI", IsUnique = true)]
+    public class Cliente
+    {
+       
+        [Required(ErrorMessage = "campo obligatorio")]
+        [MaxLength(20, ErrorMessage = "Este dato no puede tener mas de {1} caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "campo obligatorio")]
+        [MaxLength(20, ErrorMessage = "Este dato no puede tener mas de {1} caracteres")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "campo obligatorio")]
+        [MaxLength(20, ErrorMessage = "Este dato no puede tener mas de {1} caracteres")]
+        public string DNI { get; set; } 
+
+    }
+}

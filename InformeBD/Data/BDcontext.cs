9e9baf7b466caf8bda7f477c737298ada84de9fe
FelixManuelEspinoza <81267662+FelixManuelEspinoza@users.noreply.github.com>
2022-08-09 01:40:34 -0300

    Agregar archivos de proyecto.

---
+++ InformeBD/Data/BDcontext.cs
@@ -0,0 +1,26 @@
+﻿using InformeBD.entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformeBD.Data
+{
+    public class BDcontext : DbContext
+    {
+
+        public DbSet <Mecanico> Mecanicos { get; set; } 
+
+        public DbSet <Cliente> Clientes { get; set; }
+
+        public DbSet <Vehiculo> Vehiculos { get; set; }
+
+
+        public BDcontext(DbContextOptions options) : base(options)
+        {
+        
+        }
+    }
+}
---
+++ InformeBD/Data/entidades/Vehiculo.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformeBD.entidades
+{
+    [Index(nameof(Matricula), Name = "Numero de matricula del vehiculo", IsUnique = true)]
+     public class Vehiculo   
+    {
+
+        [Required(ErrorMessage = "campo obligatorio")]
+        [MaxLength(20, ErrorMessage = "Este dato no puede tener mas de {1} caracteres")]
+        public string Marca { get; set; }
+
+        [Required(ErrorMessage = "campo obligatorio")]
+        [MaxLength(20, ErrorMessage = "Este dato no puede tener mas de {1} caracteres")]
+        public string Modelo { get; set; }
+
+        [Required(ErrorMessage = "campo obligatorio")]
+        [MaxLength(20, ErrorMessage = "Este dato no puede tener mas de {1} caracteres")]
+        public string Matricula { get; set; }
+
+        [Required(ErrorMessage = "campo obligatorio")]
+        [MaxLength(500, ErrorMessage = "Este dato no puede tener mas de {1} caracteres")]
+        public string Detalles { get; set; }    
+
+
+
+    }
+}
